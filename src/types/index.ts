8208d76ed509462de9d8f05803933c91e274bcd6
KuldeepJha5176 <ranacjha@gmail.com>
2025-05-04 13:38:03 +0530

    added genric button and input componemts

---
+++ src/types/index.ts
@@ -0,0 +1,32 @@
+import { ReactElement } from "react";
+export interface ButtonProps {
+  variant:
+    | "primary"
+    | "secondary"
+    | "round"
+    | "roundchips"
+    | "new"
+    | "load"
+    | "drop"
+    | "danger";
+  children: React.ReactNode;
+  startIcon?: ReactElement;
+  endIcon?: ReactElement;
+  size: "sm" | "md" | "lg" | "vsm";
+  onClick?: () => void;
+  loading?: boolean;
+  disabled?: boolean;
+}
+
+export interface InputProps {
+    // onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    placeholder: string;
+    variant: "primary" | "secondary";
+    reference? : React.RefObject<HTMLInputElement> | undefined;
+    maxlength? : number;
+    type? : string;
+    required?: boolean;
+    onChange?: () => void;
+    onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>;
+    endICon? : ReactElement;
+  }
