40063a4020ed3a93614af8697fd1c322d5b89bf5
KuldeepJha5176 <ranacjha@gmail.com>
2025-05-17 01:45:51 +0530

    ref fixed

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,7 @@
 import { CircleX, Loader2 } from "lucide-react";
-import { Button } from './Button.tsx';
+import { Button } from './Button';
 import { Chips } from "./Chips";
-import { Input } from "./Input.tsx";
+import { Input } from "./Input";
 import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import ReactQuill from "react-quill";
